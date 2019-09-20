fac29ee4eff99bd1fa7825877ab7b3ac2ec89587
VictorHermansen <Victor_Hermansen@hotmail.com>
2019-09-20 15:21:06 +0200

    Basic character, switching and chasecam

---
+++ Unite The Goblins/Assets/CharacterManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    public GameObject[] players;
+    public Camera camera;
+    
+    void Start()
+    {
+        SetCurrentActiveCharacter(players[0]);
+    }
+
+    private void SetCurrentActiveCharacter(GameObject player)
+    {
+        for (int i = 0; i != players.Length; ++i)
+        {
+            players[i].GetComponent<CharacterMotor>().inputMoveDirection = Vector2.zero;
+            players[i].GetComponent<CharacterMotor>().inputJump = false;
+            players[i].GetComponent<PlatformInputController>().enabled = false;
+        }
+
+        camera.GetComponent<SmoothCamera>().target = player.transform;
+        player.GetComponent<PlatformInputController>().enabled = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Player1"))
+        {
+            SetCurrentActiveCharacter(players[0]);
+        }
+
+        if (Input.GetButtonDown("Player2"))
+        {
+            SetCurrentActiveCharacter(players[1]);
+        }
+
+        if (Input.GetButtonDown("Player3"))
+        {
+            SetCurrentActiveCharacter(players[2]);
+        }
+    }
+}
