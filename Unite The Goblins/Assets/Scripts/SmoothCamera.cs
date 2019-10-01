2aa7c5a7f2e0bccbe1399575f044d01f1f044cf0
Ola <ola.lidberg@gmail.com>
2019-10-01 11:20:05 +0200

    proto lvl extended

--- Unite The Goblins/Assets/Scripts/SmoothCamera.cs
+++ Unite The Goblins/Assets/Scripts/SmoothCamera.cs
@@ -16,7 +16,7 @@ public class SmoothCamera : MonoBehaviour
         if (target)
         {
             Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
-            Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+            Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.4f, 0.4f, point.z)); //(new Vector3(0.5, 0.5, point.z));
             Vector3 destination = transform.position + delta;
             transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
         }
