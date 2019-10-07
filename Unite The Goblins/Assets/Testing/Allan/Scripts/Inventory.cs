11d2514577b3c9008aa1636f84588f25e89f2487
Allan Flensburg <allwanz0r@gmail.com>
2019-10-07 17:07:57 +0200

    Add door and key prototype

---
+++ Unite The Goblins/Assets/Testing/Allan/Scripts/Inventory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    List<int> keyList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyList = new List<int>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (int i in keyList)
+        {
+            Debug.Log(i);
+        }
+    }
+
+    void AddKey(int ID)
+    {
+        keyList.Add(ID);
+    }
+
+    public bool ReturnKey(int ID)
+    {
+        foreach (int i in keyList)
+        {
+            if (i == ID)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
