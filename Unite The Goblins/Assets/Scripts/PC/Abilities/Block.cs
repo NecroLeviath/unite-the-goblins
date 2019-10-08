4689b7173aab7c16a764c7670f5ac6de4b2de026
NecroLeviath <36337546+NecroLeviath@users.noreply.github.com>
2019-10-08 18:58:31 +0200

    Implement basic shield ability

---
+++ Unite The Goblins/Assets/Scripts/PC/Abilities/Block.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+	bool active = false;
+	public GameObject shieldPrefab;
+	GameObject shield;
+	
+	void Start()
+	{
+		
+	}
+	
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.F))
+		{
+			//active = !active;
+			if (shield == null) //active)
+			{
+
+				var offset = (transform.forward / 2) + new Vector3(0, 1f, 0); // In front of the goblin
+				
+				shield = Instantiate(shieldPrefab, transform.position + offset, transform.rotation, transform);
+			}
+			else
+			{
+				Destroy(shield);
+			}
+		}
+	}
+}
