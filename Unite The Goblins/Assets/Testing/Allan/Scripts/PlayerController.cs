ad052f9d05233b83bc3e65027dce8c18e17ac936
Allan Flensburg <allwanz0r@gmail.com>
2019-10-17 10:36:29 +0200

    New Player controller (first version)

---
+++ Unite The Goblins/Assets/Testing/Allan/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    bool abilityInUse;
+    CharacterManager cm;
+    GameObject g;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        abilityInUse = false;
+        cm = transform.GetComponent<CharacterManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (g == null)
+        {
+            g = cm.ReturnCurrentCharacter();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) || Input.GetButtonDown("PS4_O"))
+        {
+            g.SendMessage("Message", "UseShapeshift");
+        }
+    }
+
+    public void AbilityIsUsed(bool b)
+    {
+        abilityInUse = b;
+    }
+
+    public bool ReturnStatus()
+    {
+        return abilityInUse;
+    }
+}
