7e3108e8c0ac5dc93795d802366bc7d9338afec3
Allan Flensburg <allwanz0r@gmail.com>
2019-11-04 09:40:27 +0100

    Bridge prefab added (needs button)

--- Unite The Goblins/Assets/Testing/Allan/Scripts/Misc scripts/LowerBridge.cs
+++ Unite The Goblins/Assets/Testing/Allan/Scripts/Misc scripts/LowerBridge.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class LowerBridge : MonoBehaviour
 {
     bool isActive;
-    float rotateSpeed = 25.0f;
+    public int rotateSpeed = 2;
+    public float smooth = 1f;
     bool lowerBridge = false;
+    Quaternion targetRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         isActive = false;
+        targetRotation = transform.rotation;
+        targetRotation *= Quaternion.AngleAxis(90, new Vector3(1, 0, 0));
     }
 
     private void Update()
     {
-        if (isActive && gameObject.transform.rotation.eulerAngles.x >= 0 && gameObject.transform.rotation.eulerAngles.x < 89.9f)
+        if (isActive/* && gameObject.transform.rotation.eulerAngles.x >= 0 && gameObject.transform.rotation.x < 90.0f*/)
         {
-            gameObject.transform.Rotate(Time.deltaTime * rotateSpeed, 0, 0);
+            //gameObject.transform.Rotate(Time.deltaTime * rotateSpeed, 0, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotateSpeed * smooth * Time.deltaTime);
         }
 
         //Debug.Log(gameObject.transform.rotation.eulerAngles.x);
